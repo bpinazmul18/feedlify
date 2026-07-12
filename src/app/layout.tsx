1fc70d4a5229a1d8544a36a2def0a778a42acd63
Nazmul Haque <bpinazmul18@gmail.com>
2026-07-12 21:11:21 +0600

    Add responsive styles for various screen sizes in responsive.css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,11 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import "@/styles/bootstrap.min.css";
+import "@/styles/common.css";
+import "@/styles/main.css";
+import "@/styles/responsive.css";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
